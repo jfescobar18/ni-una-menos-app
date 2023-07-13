e39704db3528e7365a78d8089eb9d0f710165734
Francisco Escobar <jfescobar18@hotmail.com>
2023-07-13 02:21:06 -0600

    fix UI

--- src/containers/MenuContainer/styledComponents.tsx
+++ src/containers/MenuContainer/styledComponents.tsx
@@ -1,9 +1,21 @@
 import styled from "styled-components"
 
+export const ParentContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    background-color: #5f0f5f;
+    flex-direction: column;
+`
+
 export const ButtonContainer = styled.div`
     display: flex;
     flex-direction: column;
     position: absolute;
-    top: calc(50% - 63px);
+    top: calc(50% - ${(props) => props.inlist * 32}px);
     bottom: 50%;
 `
