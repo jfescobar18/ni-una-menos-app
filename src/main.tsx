bd7231f3d700d7a9fc2c80a666ae1dec8426d5a4
Francisco Escobar <jfescobar18@hotmail.com>
2023-07-13 02:22:20 -0600

    add store

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,22 @@ import ReactDOM from "react-dom/client"
 import LoginPage from "./pages/LoginPage"
 import MenuPage from "./pages/MenuPage"
 import DashboardPage from "./pages/DashboardPage"
+import { legacy_createStore as createStore } from "redux"
+import activeDeviceReducer from "./app/activeDevice/reducer"
+import { Provider } from "react-redux"
+
+const store = createStore(activeDeviceReducer)
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/menu" element={<MenuPage />} />
-                <Route path="/dashboard" element={<DashboardPage />} />
-            </Routes>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/menu" element={<MenuPage />} />
+                    <Route path="/dashboard" element={<DashboardPage />} />
+                </Routes>
+            </Router>
+        </Provider>
     </React.StrictMode>
 )
