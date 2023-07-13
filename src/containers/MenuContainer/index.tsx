fbfac93939c8fa70ad8ed076441f17da6a58e628
Francisco Escobar <jfescobar18@hotmail.com>
2023-07-13 02:21:18 -0600

    mock devices and IDs

--- src/containers/MenuContainer/index.tsx
+++ src/containers/MenuContainer/index.tsx
@@ -1,9 +1,29 @@
 import { Button } from "../../components/Button"
 import Header from "../../components/Header"
-import { ParentContainer } from "../LoginContainer/styledComponents"
-import { ButtonContainer } from "./styledComponents"
+import { ParentContainer, ButtonContainer } from "./styledComponents"
+import { MariaDevice } from "../../types/MariaDevice"
+import { useDispatch } from "react-redux"
+import { setActiveDevice } from "../../app/activeDevice/actions"
 
 const MenuContainer = () => {
+    const dispatch = useDispatch()
+
+    const mockedDevices: MariaDevice[] = [
+        { id: "001", name: "Francisco", alias: "Paco" },
+        { id: "002", name: "Fernanda", alias: "Fer" },
+        { id: "003", name: "Mariana", alias: "Mariana" },
+        { id: "004", name: "Dante", alias: "Dan" },
+        { id: "005", name: "Alvaro", alias: "Alvaro" },
+    ]
+
+    const updateCurrentDevice = (id: string) => {
+        const filteredDevice = mockedDevices.find((device) => device.id === id)
+
+        if (filteredDevice) {
+            dispatch(setActiveDevice(filteredDevice))
+        }
+    }
+
     return (
         <>
             <ParentContainer>
@@ -13,23 +33,20 @@ const MenuContainer = () => {
                     size="m"
                     text="Seleccione un dispositivo"
                 />
-                <ButtonContainer>
-                    <Button
-                        to="/Dashboard"
-                        text="Dispositivo 1"
-                        bgcolor="#fff"
-                        textcolor="#5f0f5f"
-                        width="300px"
-                        size="s"
-                    />
-                    <Button
-                        to="/Dashboard"
-                        text="Dispositivo 2"
-                        bgcolor="#fff"
-                        textcolor="#5f0f5f"
-                        width="300px"
-                        size="s"
-                    />
+                <ButtonContainer inlist={mockedDevices.length}>
+                    {mockedDevices.map((device) => {
+                        return (
+                            <Button
+                                to="/Dashboard"
+                                onClick={() => updateCurrentDevice(device.id)}
+                                text={`Dispositivo de ${device.alias}`}
+                                bgcolor="#fff"
+                                textcolor="#5f0f5f"
+                                width="300px"
+                                size="s"
+                            />
+                        )
+                    })}
                 </ButtonContainer>
             </ParentContainer>
         </>
